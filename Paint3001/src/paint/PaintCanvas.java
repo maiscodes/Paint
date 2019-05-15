20fd61838d776d592614cf9898bf566956c399a8
Sam Holden <samuel99holden@gmail.com>
2019-05-15 11:32:26 +1000

    added gc to canvas

--- Paint3001/src/paint/PaintCanvas.java
+++ Paint3001/src/paint/PaintCanvas.java
@@ -5,11 +5,17 @@ import javafx.scene.canvas.*;
 public class PaintCanvas extends Canvas {
 
     private int pixels;
+    private GraphicsContext gc;
 
     public PaintCanvas(int pixels) {
         this.pixels = pixels;
         super.setWidth(pixels);
         super.setHeight(pixels);
+        this.gc = this.getGraphicsContext2D();
+    }
+
+    public GraphicsContext getGraphicsContext(){
+        return gc;
     }
 }
 
