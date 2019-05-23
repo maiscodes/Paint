4d8af90f5b911676635ba3936b4e4debb07f2a16
Sam Holden <samuel99holden@gmail.com>
2019-05-23 10:10:41 +1000

    Create ReadTest.java

---
+++ Paint3001/src/paint/ReadTest.java
@@ -0,0 +1,12 @@
+package paint;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReadTest {
+
+    @BeforeEach
+    public void setUpFile(){
+
+    }
+}
