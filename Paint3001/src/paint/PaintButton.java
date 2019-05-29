a7848b463929fafca305b70f7d02c1d4f6a06c58
Unknown <maisievuong@gmail.com>
2019-05-29 15:15:47 +1000

    Javadoc PaintButton

--- Paint3001/src/paint/PaintButton.java
+++ Paint3001/src/paint/PaintButton.java
@@ -4,8 +4,16 @@ import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 
-// could be further divided
+
 public class PaintButton extends Button {
+    /**
+     * Given a ShapeType enum and canvas object it creates a button
+     * which the user will click on to choose the current shape type
+     * they want to draw on the canvas.
+     *
+     * @param shapeType enumerator representing the specific drawing tool
+     * @param canvas canvas which this particular shape will be drawn on
+     */
     public PaintButton(ShapeType shapeType, PaintCanvas canvas) {
         super.setText(shapeType.toString());
         addEventHandler(MouseEvent.MOUSE_PRESSED,
