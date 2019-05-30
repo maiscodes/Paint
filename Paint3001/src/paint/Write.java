b6e49bc72fce6f54c04482ed0fef1ce998d7bd19
Sam Holden <samuel99holden@gmail.com>
2019-05-30 12:03:34 +1000

    Write javadoc

--- Paint3001/src/paint/Write.java
+++ Paint3001/src/paint/Write.java
@@ -6,6 +6,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Write {
+    /**
+     * Given a PaintCanvas and File a .vec file will be made with instructions
+     * from the given PaintCanvas and output to given File path
+     *
+     * @param canvas a PaintCanvas to take instructions from
+     * @param file a File with path to write to
+     */
     public static void write(PaintCanvas canvas, File file){
         BufferedWriter bw = null;
         try {
